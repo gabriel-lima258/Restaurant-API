2dba37b5b06850bc7e014b7f46d1d2a5f4455d82
Gabriel Lima <gabriel58221@gmail.com>
2025-11-27 21:57:00 -0300

    first commit

---
+++ src/main/java/com/gtech/food_api/domain/model/Kitchen.java
@@ -0,0 +1,42 @@
+package com.gtech.food_api.domain.model;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+public class Kitchen {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Kitchen kitchen = (Kitchen) o;
+        return Objects.equals(id, kitchen.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
---
+++ src/main/java/com/gtech/food_api/domain/model/Restaurant.java
@@ -0,0 +1,51 @@
+package com.gtech.food_api.domain.model;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+public class Restaurant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String shippingFee;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getShippingFee() {
+        return shippingFee;
+    }
+
+    public void setShippingFee(String shippingFee) {
+        this.shippingFee = shippingFee;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Restaurant that = (Restaurant) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
